5afbd3e12a7f3c32950ff75ecbf9857cfdedd1ea
Lalit0101 <lalitkp0101@gmail.com>
2024-08-09 18:28:40 +0530

    okk

--- project_1/scrpit.js
+++ project_1/scrpit.js
@@ -1,18 +1,18 @@
-const months = document.getElementById("months");
-const days = document.getElementById("days");
-const dates = document.getElementById("date");
-const year = document.getElementById("year");
+// const months = document.getElementById("months");
+// const days = document.getElementById("days");
+// const dates = document.getElementById("date");
+// const year = document.getElementById("year");
 
-const date = new Date();
-const month = date.getMonth();
-months.innerHTML = date.toLocaleString("en", {
-    month : "long"
-});
+// const date = new Date();
+// const month = date.getMonth();
+// months.innerHTML = date.toLocaleString("en", {
+//     month : "long"
+// });
 
-days.innerHTML = date.toLocaleString("en", {
-    weekday:"long"
-});
+// days.innerHTML = date.toLocaleString("en", {
+//     weekday:"long"
+// });
 
-dates.innerHTML = date.getDate();
+// dates.innerHTML = date.getDate();
 
-year.innerHTML = date.getFullYear();
+// year.innerHTML = date.getFullYear();

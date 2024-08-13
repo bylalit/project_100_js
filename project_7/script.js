7df7abb8df31f3a559249a6aaa20d8ff35f4c4f9
Lalit0101 <lalitkp0101@gmail.com>
2024-08-13 19:46:21 +0530

    okkk done the project

--- project_7/script.js
+++ project_7/script.js
@@ -53,15 +53,22 @@ const innertext = document.getElementById("demo");
 
 btn1.addEventListener("click", dispaly);
 
+let totalAmount = 0;
 
 function dispaly(){
+    let amountValue = parseFloat(amountItem.value);
     let newEl = document.createElement("p");
-    if (nameItem.value.trim() === "" || amountItem.value.trim() === ""){
+    if (nameItem.value.trim() === "" || isNaN(amountValue)){
         alert("Please Enter the value!");
     }else{
-        newEl.innerHTML += `${nameItem.value}: ₹${amountItem.value}.00`;
+        
+        newEl.innerHTML += `${nameItem.value}: ₹${amountValue}.00`;
+
+        totalAmount += amountValue;
+        document.getElementById("totalAmount").innerHTML = `Total Amount: ₹${totalAmount}.00`;
+        innertext.appendChild(newEl);
+
         nameItem.value = "";
         amountItem.value = "";
-        innertext.appendChild(newEl);
     };
 };

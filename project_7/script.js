3200434a7a08070a7cbfe19721f0fcd96507b001
Lalit0101 <lalitkp0101@gmail.com>
2024-08-13 20:05:27 +0530

    done the project

--- project_7/script.js
+++ project_7/script.js
@@ -1,48 +1,3 @@
-// let nameItem = document.getElementById("nameItem");
-// let amountItem = document.getElementById("amountItem");
-
-
-// const add = document.getElementById("add");
-// const remove = document.getElementById("remove");
-
-// let total = document.getElementById("total");
-
-
-
-
-// add.addEventListener("click", function(){
-//     let a = nameItem.value;
-//     let b = amountItem.value;
-
-//     let store = document.getElementById("demo");
-//     let p = document.createElement("p");
-
-//     p.classList.add("dispaly");
-//     p.innerHTML += `${a}: Rs.${b}`;
-
-//     store.appendChild(p);
-//     // console.log(store);
-//     p.style.display = "block";
-
-
-//     let d = 0;
-//     d += parseInt(d + b)
-//     total.innerHTML = d;
-
-//     remov(p);
-// });
-
-// function remov(p){
-//     remove.addEventListener("click", function(){
-//         p.innerHTML = "";
-//         p.style.display = "none";
-//         total.innerHTML = "";
-//     })
-// }
-
-
-
-
 const nameItem = document.getElementById("nameItem");
 const amountItem = document.getElementById("amountItem");
 
@@ -72,3 +27,13 @@ function dispaly(){
         amountItem.value = "";
     };
 };
+
+
+btn2.addEventListener("click", clearer);
+
+function clearer(){
+    innertext.innerHTML = "";
+
+    totalAmount = 0;
+    document.getElementById("totalAmount").innerHTML = `Total Amount: ₹0.00`;
+}

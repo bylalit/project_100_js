4f66482a9b927314dbbf572fdbd260d7dd66c894
Lalit0101 <lalitkp0101@gmail.com>
2024-08-15 20:24:16 +0530

    okk done the

--- project_10/script.js
+++ project_10/script.js
@@ -1,28 +1,28 @@
 const imageContainerEl = document.querySelector(".image-cointainer");
 
-
 const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
 
 let x = 0;
 let timer;
-prevEl.addEventListener("click", ()=>{
-    x += 45;
-    clearTimeout(timer);
-    updateGalley();
+
+prevEl.addEventListener("click", () => {
+  x += 45;
+  clearTimeout(timer);
+  updateGalley();
 });
-nextEl.addEventListener("click", ()=>{
+
+nextEl.addEventListener("click", () => {
+  x -= 45;
+  clearTimeout(timer);
+  updateGalley();
+});
+
+function updateGalley() {
+  imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+  timer = setTimeout(() => {
     x -= 45;
-    clearTimeout(timer);
     updateGalley();
-});
-
-function updateGalley(){
-    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
-    timer = setTimeout(()=>{
-        x -= 45;
-        updateGalley();
-    }, 3000);
+  }, 3000);
 }
-
 updateGalley();

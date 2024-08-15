11814e0c2752075c1c9baf3bc6fbcb4e668c7ef1
Lalit0101 <lalitkp0101@gmail.com>
2024-08-15 20:20:09 +0530

    done the

--- project_10/script.js
+++ project_10/script.js
@@ -5,11 +5,24 @@ const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
 
 let x = 0;
+let timer;
 prevEl.addEventListener("click", ()=>{
     x += 45;
+    clearTimeout(timer);
+    updateGalley();
+});
+nextEl.addEventListener("click", ()=>{
+    x -= 45;
+    clearTimeout(timer);
     updateGalley();
 });
 
 function updateGalley(){
     imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=>{
+        x -= 45;
+        updateGalley();
+    }, 3000);
 }
+
+updateGalley();

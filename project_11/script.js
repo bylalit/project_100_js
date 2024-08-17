73b45056f48fa3d373a35c204a0b8c51913483fb
Lalit0101 <lalitkp0101@gmail.com>
2024-08-17 09:21:00 +0530

    complate project

--- project_11/script.js
+++ project_11/script.js
@@ -7,24 +7,26 @@ const newYearTime = new Date("Jan 1, 2025 00:00:00").getTime();
 
 updateCountdown();
 
-function updateCountdown(){
-    const now = new Date().getTime();
-    // console.log(now, newYearTime);
-    const gap = newYearTime - now;
-    // console.log(gap);
+function updateCountdown() {
+  const now = new Date().getTime();
+  // console.log(now, newYearTime);
+  const gap = newYearTime - now;
+  // console.log(gap);
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const d = Math.floor(gap / day);
-    const h = Math.floor((gap % day) / hour);
-    const m = Math.floor((gap % hour) / minute);
-    const s = Math.floor((gap % minute) / second);
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
 
-    dayEl.innerText = d;
-    hourEl.innerText = h;
-    minutEl.innerText = m;
-    secondEl.innerText = s;
+  dayEl.innerText = d;
+  hourEl.innerText = h;
+  minutEl.innerText = m;
+  secondEl.innerText = s;
+
+  setTimeout(updateCountdown, 1000);
 }

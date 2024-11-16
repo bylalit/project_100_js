3fa74d41804d452dc545c35906e1c9391fc39128
Lalit0101 <lalitkp0101@gmail.com>
2024-11-16 22:24:54 +0530

    done

--- project_12/script.js
+++ project_12/script.js
@@ -1,13 +1,13 @@
 const btn = document.getElementById("btn");
 
-// btn.addEventListener("click", function () {
-//   let input = document.getElementById("input").value;
-//   if (input.length > 0) {
-//     let output = document.getElementById("output");
-//     output.innerHTML = input.toUpperCase();
-//     output.style.color = "blue";
-//   } else {
-//     alert("Please Enter The Your Birthday Date");
-//   }
-// });
+btn.addEventListener("click", function () {
+  let input = document.getElementById("input").value;
+  if (input.length > 0) {
+    let output = document.getElementById("output");
+    output.innerHTML = input.toUpperCase();
+    output.style.color = "blue";
+  } else {
+    alert("Please Enter The Your Birthday Date");
+  }
+});
 
